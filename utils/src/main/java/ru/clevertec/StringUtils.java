a8dbb9df4792044c12c2c673f640b318e69d0e5a
Nikolay1992167 <wulik1081@mail.ru>
2023-10-17 10:01:40 +0300

    feat: creating an archive utils-1.3.5.jar and publishing it to a local repository

---
+++ utils/src/main/java/ru/clevertec/StringUtils.java
@@ -0,0 +1,14 @@
+package ru.clevertec;
+
+import java.math.BigDecimal;
+
+public class StringUtils {
+
+    private StringUtils() {
+    }
+
+    public static boolean isPositiveNumber(String str) {
+        return org.apache.commons.lang3.StringUtils.isNumeric(str) &&
+                BigDecimal.ZERO.compareTo(new BigDecimal(str)) < 1;
+    }
+}
